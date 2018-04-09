5cadec63b71dcbb7bb3a83e09dd85b57b90e8c94
Rafael Jhonatan <rafaeljhonatan111@gmail.com>
2018-04-09 08:46:45 -0400

    Correção do nome do arquivo de configuraca do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+
+
 module.exports = {
+    mode: 'development',
     context: __dirname + '/app',
     entry: './src/app.js',
     output :{
         path: path.resolve(__dirname , 'dist'),
         filename: 'bundle.js'
-    }
-    };
+    },
+    devServer:{
+        contentBase: 'app'
+    },
+    plugins: [
+        new LiveReloadPlugin()
+    ]
+};
