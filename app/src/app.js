bf9c580b532aa986e69c991094df47acb65ed4d7
Rafael Jhonatan <rafaeljhonatan111@gmail.com>
2018-04-09 17:48:30 -0400

    Adiciona o routes ao projeto as configurações de view

--- app/src/app.js
+++ app/src/app.js
@@ -1,13 +1,13 @@
-import angular from 'angular'
-
-
+import angular from 'angular';
+import ngRoute from 'angular-route'
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import $ from 'jquery';
 import 'bootstrap';
 import css from './style/app.css';
-// import listaTelefonicaController from './controllers/listaTelefonicaController.js';
-$('.dropdown-toggle').dropdown();
-var ngModule = angular.module('app', []);
-ngModule.controller('listaTelefonicaController',['$scope',function ($scope){
-    $scope.test = 'Hola!';
-}]);
+import listaTelefonicaController from './js/controllers/ListaTelefonicaController';
+import configRoutes from './js/config/routerConfig'
+
+var ngModule = angular.module('app', ['ngRoute']);
+console.log(configRoutes);
+ngModule.config(configRoutes);
+ngModule.controller('listaTelefonicaController',listaTelefonicaController);
